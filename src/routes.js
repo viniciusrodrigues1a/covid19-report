116fcb653e3e4b9e1e077716110b18b20a2c5357
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-26 10:44:17 -0300

    :sparkles: :bug: changing BrowserRouter to HashRouter
    Netlify was giving the error: "Page not found" when trying to reach any route, for example: https://covid19-report.netlify.com/Brazil

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 
 export default function () {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/" component={Main} />
         <Route path="/:country" component={Main} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
