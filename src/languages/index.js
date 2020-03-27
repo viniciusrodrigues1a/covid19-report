084b6f7676333d2759edeb7351855f95b72ccf6c
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-27 04:30:42 -0300

    :sparkles: adding a language name

--- src/languages/index.js
+++ src/languages/index.js
@@ -3,6 +3,6 @@ import en_US from './en_US';
 import pt_BR from './pt_BR';
 
 export default {
-  US: { countryCode: 'US', ...en_US },
-  BR: { countryCode: 'BR', ...pt_BR },
+  US: { countryCode: 'US', languageName: 'English', ...en_US },
+  BR: { countryCode: 'BR', languageName: 'Brazilian Portugese', ...pt_BR },
 };
